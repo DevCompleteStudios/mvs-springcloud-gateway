bd4c7ee33098f4d278cef37ace0fa62098975592
Jorge Yael Chino Bolaños <jorge.yael333@gmail.com>
2024-12-03 17:30:39 -0600

    First commit

---
+++ src/main/java/com/yael/springcloud/app/gateway/mvs_gateway/MvsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.yael.springcloud.app.gateway.mvs_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvsGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yael/springcloud/app/gateway/mvs_gateway/MvsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yael.springcloud.app.gateway.mvs_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
