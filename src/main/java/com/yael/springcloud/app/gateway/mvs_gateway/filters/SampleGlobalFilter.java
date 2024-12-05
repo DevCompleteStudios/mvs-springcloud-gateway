18d08687aeac34763c87ff35720def2d7db42862
Jorge Yael Chino Bolaños <jorge.yael333@gmail.com>
2024-12-05 11:29:07 -0600

    Agregando filtro global

---
+++ src/main/java/com/yael/springcloud/app/gateway/mvs_gateway/filters/SampleGlobalFilter.java
@@ -0,0 +1,35 @@
+package com.yael.springcloud.app.gateway.mvs_gateway.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+
+
+@Component
+public class SampleGlobalFilter implements GlobalFilter {
+
+
+    private final Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
+    
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("jecutando el filtro antes del request PRE");
+
+        return chain.filter(exchange)
+            .then(Mono.fromRunnable( () -> {
+                logger.info("Ejecutando filtro post response");
+
+                exchange.getResponse().getCookies().add("color", ResponseCookie.from("Color", "Red").build());
+                exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+            }));
+    }
+
+}
